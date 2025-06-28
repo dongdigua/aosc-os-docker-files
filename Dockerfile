323ebd2feba78423e8f906b4113c522a4acd94cb
dongdigua <dongdigua@outlook.com>
2025-06-28 13:34:17 +0800

    add buildkit variant #5

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM scratch
 LABEL maintainer="AOSC-dev"
 ARG TARGETOS
 ARG TARGETARCH
-ADD "${TARGETARCH}.tar" /
+ADD "${TARGETARCH}.tar.xz" /
 CMD ["/bin/bash"]
 RUN sed -i 's/no_check_dbus = false/no_check_dbus = true/' /etc/oma.toml
---
+++ get_buildkit.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+function get_buildkit() {
+  if [[ "x$1" == 'x' ]]; then
+    echo 'provide ARCH pls'
+    exit 1
+  fi
+
+  OUTPUT="$1.tar.xz"
+
+  URL_AND_HASH=$(curl https://releases.aosc.io/manifest/recipe.json \
+                   | jq --raw-output ".variants | map(select(.name==\"BuildKit\" and .retro==false)) | .[0].tarballs | map(select(.arch==\"$1\")) | sort_by(.date) | reverse .[0] | .path, .sha256sum")
+  # e.g.: os-amd64/buildkit/aosc-os_buildkit_20250606_amd64.tar.xz
+
+  {
+    read -r URL_PATH
+    read -r HASH
+  } <<< "${URL_AND_HASH}"
+
+
+  curl "https://releases.aosc.io/${URL_PATH}" -o "${OUTPUT}"
+
+  echo "${HASH} ${OUTPUT}" | sha256sum -c -
+}
+
+ARCHS=(amd64 arm64 riscv64 loongarch64)
+
+for arch in "${ARCHS[@]}"
+do
+  get_buildkit $arch
+done
